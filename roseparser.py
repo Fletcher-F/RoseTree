fff0ffc98a3a256ac12324f23dc08f1190727f8e
Fletcher Falk <145166065+Fletcher-F@users.noreply.github.com>
2025-05-08 12:12:50 -0700

    Add files via upload

---
+++ roseparser.py
@@ -0,0 +1,44 @@
+"""XML Parser for Rosetree
+Writes the results of blast from XML into FASTA for alignment and phylogeny.
+Developed by: Fletcher Falk"""
+
+from xml.etree import ElementTree as ET
+
+"""XML Parser Function"""
+def parsexml(path):
+    print("Writing result sequences (100) into new fasta file for phylogeny construction...", "\n")
+    xmltree = ET.parse("blast.xml")
+    root = xmltree.getroot()
+    
+    """Read contents from input sequence to append in"""
+    inputsequence = open(path, "r")
+
+    """Open file and write input sequence"""
+    newfasta = open("blastresults.fasta", "a")
+    for line in inputsequence:
+        newfasta.write(line)
+    newfasta.write('\n')
+
+    """Write rest of blast results into file"""
+    for hit in root.findall('.//Hit'):
+        hit_id = hit.findtext('Hit_accession')
+        hit_def = hit.findtext('Hit_def')
+        hsp_seq = hit.findtext('.//Hit_hsps/Hsp/Hsp_hseq')
+
+        """Ensure fasta format"""
+        newfasta.write(">")
+        newfasta.write(hit_id)
+        newfasta.write(" ")
+        newfasta.write(hit_def)
+        newfasta.write('\n')
+        newfasta.write(hsp_seq)
+        newfasta.write('\n')
+    newfasta.close()
+
+    """Check number of lines written, should be 202"""
+    writecheck = open("blastresults.fasta", "r")
+    checklines = len(writecheck.readlines())
+    writecheck.close()
+
+    print("Finished parsing.. ", "Wrote: ", checklines, " lines to document blastresults.fasta", "\n")
+
