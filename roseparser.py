3025ead50acbd830c56b4f92eafbf5f1c6eb9cf4
Fletcher Falk <145166065+Fletcher-F@users.noreply.github.com>
2025-05-09 13:47:13 -0700

    return values for metadata parsing

--- roseparser.py
+++ roseparser.py
@@ -9,15 +9,20 @@ def parsexml(path):
     print("Writing result sequences (100) into new fasta file for phylogeny construction...", "\n")
     xmltree = ET.parse("blast.xml")
     root = xmltree.getroot()
+    hit_list = []
     
     """Read contents from input sequence to append in"""
     inputsequence = open(path, "r")
+    inputblast = inputsequence.readline()
 
     """Open file and write input sequence"""
     newfasta = open("blastresults.fasta", "a")
+    """Since readline earlier put first line into file instead of open and close"""
+    newfasta.write(inputblast)
     for line in inputsequence:
         newfasta.write(line)
     newfasta.write('\n')
+    inputsequence.close()
 
     """Write rest of blast results into file"""
     for hit in root.findall('.//Hit'):
@@ -33,6 +38,10 @@ def parsexml(path):
         newfasta.write('\n')
         newfasta.write(hsp_seq)
         newfasta.write('\n')
+
+        """Add accession to list for metadata"""
+        hit_list.append(hit_id)
+
     newfasta.close()
 
     """Check number of lines written, should be 202"""
@@ -41,4 +50,6 @@ def parsexml(path):
     writecheck.close()
 
     print("Finished parsing.. ", "Wrote: ", checklines, " lines to document blastresults.fasta", "\n")
-
+    
+    """Return info for metadata after parsing"""
+    return hit_list, inputblast
