36fb4a43b2e87bf93bd41de6d5e15a4df7bd4d03
Fletcher Falk <145166065+Fletcher-F@users.noreply.github.com>
2025-05-13 15:07:54 -0700

    reformat, clean, and add multisequence functionality and arguments

--- rosetree.py
+++ rosetree.py
@@ -1,20 +1,20 @@
+"""Program Info: RoseTree (VIU Summer 2025 USRA Project)
+Developed by: Fletcher Falk
+April. 28th, 2025
+Program blasts a given input sample to NCBI and constructs a phylogeny based on results.
+Metadata from NCBI is also parsed and used to supplement phylogeny output."""
+
 """Importing libraries"""
-import os, subprocess, argparse, sys, Bio
+import os, subprocess, argparse, sys
 from pathlib import Path
 from Bio import Blast, Entrez
 from Bio.Seq import Seq
-from phytreeviz import TreeViz
-from roseparser import parsexml
+from roseparser import parsexml, multiparsexml, linecheck
 from rosemetadata import metaparser
+from rosephylogeny import phylogeny
+from roseblast import nuc_blast, multi_blast
 
-"""Program Info: RoseTree (VIU Summer 2025 USRA Project)
-Developed by: Fletcher Falk
-Program blasts a given input sample to NCBI and constructs a phylogeny based on results.
-Metadata from NCBI is also parsed and used to supplement phylogeny output.
-
-April. 28th, 2025
-
-Function to execute bash commands"""
+"""Function to execute bash commands"""
 def execute(command):
     result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
     print(result.stdout, "\n", result.stderr)
@@ -28,87 +28,106 @@ def argument_parser():
     parser.add_argument("--threads", "-t",
                         help="Specify total number of threads to use. Default is 1.", default=1)
     parser.add_argument("--email", "-e",
-                        help="BLAST API requires an email in case of error or missue.", required=True)
+                        help="BLAST API requires an email in case of error.", required=True)
+    parser.add_argument("--blasttotal", "-b",
+                        help="Specify total number of blast results to use. Default is 50.", default=50)
+    parser.add_argument("--multiplesequencemode", "-ms",
+                        help="Turn on multiple sequence mode to allow multiple inputs. Takes a directory of fasta files and blasts all taking default results.", default=False)
+    parser.add_argument("--multiblasttotal", "-mb",
+                        help="Specify total number of blast results to use when running multisequencemode. Default is 10.", default=10)
+    """Return arguments"""
     return parser.parse_args()
-
-"""Blasts fasta file to NCBI
-Uses qblast from biopython library"""
-def nuc_blast(path):
-    blastseq = open(path).read()
-    """Blast sequence to nucleotide database"""
-    blast_results = Blast.qblast("blastn", "nt", blastseq, hitlist_size = 100)
-
-    """Write results into xml"""
-    with open("blast.xml", "wb") as out_stream:
-        out_stream.write(blast_results.read())
-    blast_results.close()
 
 """Runs the RoseTree program"""
 def rosetree(args):
     """Assign arguments"""
     path = args.input
-    threads= args.threads
+    threads = args.threads
+    blastnum = args.blasttotal
+    multiblastnum = args.multiblasttotal
+    """stored as string"""
+    mode = args.multiplesequencemode 
     Blast.email = args.email
     Entrez.email = args.email
+
+    """Multiple Sequence Mode Checks"""
+    if mode == "True":
+        if Path(path).is_dir() == False:
+            print("Error... Multiple sequence mode is enabled, therefore path requires directory.")
+            sys.exit()
+        files = os.listdir(path)
+        for file in files:
+            if os.path.splitext(file)[1] not in (".fasta", ".fa"):
+                print("File in directory has invalid format...\n" "Requires .fasta or .fa")
+                sys.exit()
+    else: 
+        """Check if file exists before starting"""
+        if Path(path).is_file() == False:
+            print("Error... File does not exist, exiting.")
+            sys.exit()
+        """Check valid fasta file path"""
+        if os.path.splitext(path)[1] not in (".fasta", ".fa"):
+            print("Invalid file format...\n" "Requires .fasta or .fa")
+            sys.exit()
+
+    """Start"""
+    print("--- Running RoseTree v0.1 --- \n", "Using ", threads, " threads... \n", "Be sure to checkout my GitHub :) \n", 
+    "https://github.com/Fletcher-F \n", "File(s) path = ", path, "\n", "Note: if rerunning program, ensure you are working in a new directory or move old output.\n",
+    "Results are appended and will cause errors in downstream steps.\n\n", "Note: ensure fasta input is only 2 lines: one for title, one for sequence.\n\n"
+    "Running qblast using Biopython on nucleotide database...\n")
+
+    if mode == "True":
+        print("Running in multiple sequence mode, taking top ", multiblastnum, " results from each input...\n")
+        if blastnum != 50:
+            print("Notice: you set the total blast number argument.\n You are running in multisequencemode",
+                  " if you want to change the blast number set it instead with -mb")
+        multi_blast(path, multiblastnum)
+        hit_list, inputblast = multiparsexml(path)    
+        """Convert input blast to string for rest of program"""
+        if type(inputblast) is list:
+            inputblast = ", ".join(str(x) for x in inputblast)
+            inputblast = inputblast.replace(">", "")
     
-    """Check if file exists before starting"""
-    validfile = Path(path)
-    if validfile.is_file() == False:
-        print("Error... File does not exist, exiting.")
-        sys.exit()
-
-    """Check fasta file path to start blast"""
-    validfasta = os.path.splitext(args.input)
-    if validfasta[1] in (".fasta", ".fa"):
-        print("--- Running RoseTree v0.1 --- \n", "Using ", threads, " threads... \n", "Be sure to checkout my GitHub :) \n", 
-        "https://github.com/Fletcher-F \n", "File path = ", path, "\n", "Note: if rerunning program, ensure it is in new directory or move old output as output is appended and will cause errors in downstream steps.", 
-        "\n\n", "Running qblast using Biopython on nucleotide database...")
-
+    else:
         """Blast fasta to NCBI"""
-        nuc_blast(path)
-
+        print("Taking top ", blastnum, " results.\n")
+        nuc_blast(path, blastnum)
         """Parse xml into fasta"""
         hit_list, inputblast = parsexml(path)
 
-        """Pull metadata after parsing"""
-        metaparser(hit_list, inputblast)
+    """Check lines written"""
+    linecheck()
 
-        """MAFFT for sequence alignment of fasta"""
-        print("Performing alignment from results using MAFFT...")
-        print("Using L-INS-i as less than 200 sequences are being aligned. \n")
-        alignment = "mafft-linsi blastresults.fasta > alignedresults.fasta"
-        execute(alignment)
+    """Pull metadata after parsing"""
+    metaparser(hit_list, inputblast)
 
-        """Phylogeny construction
-        Starting with RAxML as it is easy to run:
-        Consider looking at BEAST
-        Note: This tree is maximum likelihood not a bayesian tree"""
-        print("Building maximum likelihood tree with RAxML all-in-one analysis...")
-        mltree = "raxml-ng --all --msa alignedresults.fasta --model LG+G8+F --tree pars{10} --bs-trees 200 --threads " + threads
-        execute(mltree)
+    """MAFFT for sequence alignment of fasta"""
+    print("Performing alignment from results using MAFFT...")
+    print("Using L-INS-i as less than 200 sequences are being aligned. \n")
+    alignment = "mafft-linsi blastresults.fasta > alignedresults.fasta"
+    execute(alignment)
 
-        """Clean up files"""
-        cleanup = ["rm alignedresults.fasta.raxml.r*", "rm alignedresults.fasta.raxml.mlTrees", "rm alignedresults.fasta.raxml.bootstraps", "rm alignedresults.fasta.raxml.startTree", "rm alignedresults.fasta.raxml.support" ]
-        for x in cleanup:
-            execute(x)
+    """Phylogeny construction with raxml
+    Note: This tree is maximum likelihood not a bayesian tree"""
+    print("Building maximum likelihood tree with RAxML all-in-one analysis...")
+    mltree = "raxml-ng --all --msa alignedresults.fasta --model GTR+G --tree pars{25},rand{25} --threads " + threads
+    #Long analysis option: mltree = "raxml-ng --all --msa alignedresults.fasta --model LG+G8+F --tree pars{10} --bs-trees 200 --threads " + threads
+    execute(mltree)
 
-        """Draw Tree
-        Testing with phyTreeViz tool instead of running R Studio separate
-        or importing R libraries and packages"""
-        print("Drawing final maximum likelihood tree with phyTreeViz...")
-        inputtree = "alignedresults.fasta.raxml.bestTree"
-        finaltree = TreeViz(inputtree)
+    """Clean up files"""
+    cleanup = ["rm alignedresults.fasta.raxml.r*", "rm alignedresults.fasta.raxml.mlTrees", "rm alignedresults.fasta.raxml.bootstraps", "rm alignedresults.fasta.raxml.startTree", "rm alignedresults.fasta.raxml.support" ]
+    for x in cleanup:
+        execute(x)
 
-        """Save final tree"""
-        finaltree.savefig("FinalTree.pdf", dpi=300)
- 
-        """Done"""
-        print("Final tree exported as pdf...")
-        print("Bye!")
+    """Draw Tree"""
+    print("Drawing final tree with ETE...\n", "Note: if an error occurs you may have to manually install PyQt5: as it didn't install with ete3...\n",
+    "run: pip3 install PyQt5")
+    inputtree = "alignedresults.fasta.raxml.bestTree"
+    phylogeny(inputtree, inputblast)
 
-    else:
-        """Error checking input"""
-        print("Invalid file format...", "\n", "Input was", validfasta[1], "\n", "Requires .fasta or .fa")
+    """Done"""
+    print("Final tree exported as pdf...")
+    print("Bye!")
 
 """Main: sets up arguments and runs program"""
 def main():
@@ -118,21 +137,3 @@ def main():
 """Start Program"""
 if __name__ == "__main__":
     main()
-
-"""
-THINGS TO BE DONE + brainstorming:
-1. Take the top results to be used in phylogeny?? Maybe based on percent match
-Should I consider and how will we consider an outgroup for the tree?
-
-2. How will we parse the metadata and what to add/how to add to phylogeny. 
-
-3. Also need to move alot of this code into functions and clean it up.
-Plus need error checking and input validation if it will be publically shared.
-
-Extra TreeViz settings:
-Excluded for now as now branch length or confidence exists for current plot
-See about switch when using bayesian if we shift to that.
-finaltree.show_branch_length(color="red")
-finaltree.show_confidence(color="blue")
-finaltree.show_scale_bar()
-"""
