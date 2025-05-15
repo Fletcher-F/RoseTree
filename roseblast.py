30bbe9ebf8009c952227e4d329a7d116812eb6b4
Fletcher Falk <145166065+Fletcher-F@users.noreply.github.com>
2025-05-15 09:59:51 -0700

    cleanup and reuse nuc_blast

--- roseblast.py
+++ roseblast.py
@@ -1,19 +1,18 @@
 """NCBI Blast for Rosetree
 Writes the results of blast into XML for parsing.
 Developed by: Fletcher Falk"""
-
 import os, time
 from Bio import Blast
 
 """Blasts fasta file to NCBI
 Uses qblast from Biopython library"""
-def nuc_blast(path, blastnum):
+def nuc_blast(path, blastnum, resultnum):
     blastseq = open(path).read()
     """Blast sequence to nucleotide database"""
     blast_results = Blast.qblast("blastn", "nt", blastseq, hitlist_size = blastnum)
 
     """Write results into xml"""
-    with open("blast.xml", "wb") as out_stream:
+    with open("blast" + str(resultnum) + ".xml", "wb") as out_stream:
         out_stream.write(blast_results.read())
     blast_results.close()
 
@@ -22,15 +21,14 @@ takes directory path instead"""
 def multi_blast(path, blastnum):
     blastseqs = [os.path.join(path, file) for file in os.listdir(path)]
     blastloop = 1
+
+    """For each fasta in the directory"""
     for fasta in blastseqs:
         """Blast each fasta to NCBI"""
-        blastseq = open(fasta).read()
-        blast_results = Blast.qblast("blastn", "nt", blastseq, hitlist_size = blastnum)
-        
-        """Write results into xml"""
-        with open("blast" + str(blastloop) + ".xml", "wb") as out_stream:
-            out_stream.write(blast_results.read())
-        blast_results.close()
+        nuc_blast(path, blastnum, blastloop)
         blastloop += 1
         """Fit NCBI API guidelines"""
-        time.sleep(10)
+        if fasta == blastseqs[-1]:
+            continue
+        else:
+            time.sleep(5)
