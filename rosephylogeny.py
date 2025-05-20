4c6c6c7e7c51dad6da1c5077a58416581a394e59
Fletcher Falk <145166065+Fletcher-F@users.noreply.github.com>
2025-05-20 12:31:28 -0700

    Update drawing for samples and spacing

--- rosephylogeny.py
+++ rosephylogeny.py
@@ -1,7 +1,7 @@
 """Phylogeny constructor for Rosetree
 Uses ETE3 to build a phylogeny and annotate with parsed metadata
 Developed by: Fletcher Falk"""
-import os
+import os, re
 from random import randint
 from ete3 import Tree, TreeStyle, TextFace, faces
 from xml.etree import ElementTree as ET
@@ -10,7 +10,7 @@ from xml.etree import ElementTree as ET
 def graphcolor(colortotal):
     colorlist = []
     for x in colortotal:
-        """Generates random light tone color for legibility"""
+        """Generates random calm/lighter tone color for legibility"""
         colorlist.append(('#%02x%02x%02x' % (randint(197, 240), randint(197, 240), randint(197, 240))).upper())
     return colorlist
 
@@ -18,7 +18,7 @@ def graphcolor(colortotal):
 def specieslist(xmltree):
     root = xmltree.getroot()
     specieslist = []
-    """For each species in blast generate list of unique genus"""
+    """For each species in blast generate list of unique genus to generate colors for"""
     for sequence in root.findall('.//Sequence'):
         id = sequence.findtext('Sequence_id')
         tmplist = id.split(" ")
@@ -31,35 +31,70 @@ def specieslist(xmltree):
     """Return list and color for each genus"""
     return colors, cleanlist
 
+"""Node text face fixer"""
+def textfacefix(inputlabel):
+    """If input is longer than 25 characters add newline to format better"""
+    max_length = 25
+    finalabel = ""
+    """When inputlabel is greater than 25 split and reformat"""
+    while len(inputlabel) > max_length:
+        whitespace = re.search(r"\s", inputlabel[max_length:])
+        """If whitespace is found find split point otherwise use string"""
+        if whitespace:
+            split = max_length + whitespace.start()
+        else:
+            split = len(inputlabel)
+        """Remove whitespace and add newline"""
+        finalabel += inputlabel[:split].strip() + "\n"
+        inputlabel = inputlabel[split:].strip()
+    finalabel += inputlabel
+    return finalabel
+
 """Custom_layout function per ETE manual
 Setup as a closure function to pass extra vars as custom_layout
 seems to not like extra parameters"""
-def layout_parameters(root, colors, cleanlist):
+def layout_parameters(root, colors, cleanlist, inputblast, samplenum):
     def custom_layout(node):
         """If node is leaf, generate TextFace of metadata that matches
         Additionally set background color to genus color generated"""
         if node.is_leaf():
+            """Check if node is one of the input fasta files"""
+            if node.name in inputblast:
+                nonlocal samplenum
+                inputface = TextFace("Sample: " + (str(samplenum)), tight_text=True, fgcolor = "white", fsize = 12)
+                inputface.background.color = "#F7879A"
+                faces.add_face_to_node(inputface, node, column=0, position="branch-right")
+                samplenum += 1 
+            """For each sequence in parsed xml"""
             for sequence in root.findall('.//Sequence'):
                 if node.name == sequence.findtext('Sequence_accession'):
                     id = sequence.findtext('Sequence_id').split(" ")
+                    """Get color for node"""
                     if id[0] == "uncultured":
                         position = cleanlist.index(id[1])
                     else:
                         position = cleanlist.index(id[0])
+            
+                    """Add text faces for each metadata label"""
                     face = TextFace(sequence.findtext('Sequence_id'), tight_text=True, fsize = 12)
                     face.background.color = colors[position]
                     faces.add_face_to_node(face, node, column=0, position="branch-right")
-                    face2 = TextFace(sequence.findtext('.//Source/isolation_source'), fsize = 10)
+                    face2 = TextFace(textfacefix(str(sequence.findtext('.//Source/isolation_source'))), fgcolor = "gray", fsize = 10)
+                    face2.margin_left = 15
+                    face2.margin_bottom = 10
                     faces.add_face_to_node(face2, node, column=1, aligned = True)
-                    face3 = TextFace(sequence.findtext('.//Source/geo_loc_name'), fsize = 10)
+                    face3 = TextFace(textfacefix(str(sequence.findtext('.//Source/geo_loc_name'))), fgcolor = "gray", fsize = 10)
+                    face3.margin_left = 15
+                    face3.margin_bottom = 10
                     faces.add_face_to_node(face3, node, column=2, aligned = True)
-                    """
-                    Additional metadata that is removed for now.
-                    face4 = TextFace(sequence.findtext('.//Source/db_xref'), fsize = 10)
+                    face4 = TextFace(textfacefix(str(sequence.findtext('.//Source/host'))), fgcolor = "gray", fsize = 10)
+                    face4.margin_left = 15
+                    face4.margin_bottom = 10
                     faces.add_face_to_node(face4, node, column=3, aligned = True)
-                    face5 = TextFace(sequence.findtext('.//Source/mol_type'), fsize = 10)
+                    face5 = TextFace(textfacefix(str(sequence.findtext('.//Source/db_xref'))), fgcolor = "gray", fsize = 10)
+                    face5.margin_left = 15
+                    face5.margin_bottom = 10
                     faces.add_face_to_node(face5, node, column=4, aligned = True)
-                    """
     return custom_layout
 
 """Phylogeny Function"""
@@ -70,6 +105,7 @@ def phylogeny(inputtree, inputblast):
     xmltree = ET.parse("blastmetadata.xml")
     root = xmltree.getroot()
     colors, cleanlist = specieslist(xmltree)
+    samplenum = 1
 
     """Open best tree from raxml"""
     with open (inputtree, "r") as treefile:
@@ -85,10 +121,10 @@ def phylogeny(inputtree, inputblast):
         styletree.margin_bottom = 30
 
         """Tree Title"""
-        styletree.title.add_face(TextFace("Constructed Maximum Likelihood Phylogeny from NCBI Blast of: " + inputblast, fsize=16), column=0)
+        styletree.title.add_face(TextFace("Maximum Likelihood Phylogeny from NCBI Blast of:\n" + inputblast, fsize=14), column=0)
         """Render tree
         Note: os.environ is used due to render bug"""
         os.environ["QT_QPA_PLATFORM"] = "offscreen"
-        styletree.layout_fn = layout_parameters(root, colors, cleanlist)
+        styletree.layout_fn = layout_parameters(root, colors, cleanlist, inputblast, samplenum)
         tree.render("final-tree-render.pdf", tree_style = styletree)
     treefile.close()
